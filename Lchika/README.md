abed94d6f883994de494f898ab15c001dee98c92
harhogefoo <harimanmon@gmail.com>
2018-08-08 22:48:18 +0900

    doc: update README.md

---
+++ Lchika/README.md
@@ -0,0 +1,8 @@
+
+# Exec
+```bash
+sudo node led_on.js
+```
+
+# Reference
+https://qiita.com/harhogefoo/items/0b5b67cda3e22226e9aa
--- Lchika/led_on.js
+++ Lchika/led_on.js
@@ -5,12 +5,27 @@ var gpio3 = dir + '/gpio3';
 
 fs.writeFileSync(dir + '/export', 3); 
 fs.writeFileSync(gpio3 + '/direction', 'out');
-// fs.writeFileSync(gpio3 + '/value', 1);
 
 var cnt = 0;
 
-setInterval(function(){
+function isExistFile(file) {
+  try {
+    fs.statSync(file);
+    return true
+  } catch(err) {
+    if(err.code === 'ENOENT') return false
+  }
+}
+
+var intervalFn = setInterval(function(){
   cnt++;
-  fs.writeFileSync(gpio3 + '/value', cnt % 2);
-  // fs.writeFileSync(dir + '/unexport', 3);
+  if (isExistFile(gpio3)) {
+    fs.writeFileSync(gpio3 + '/value', cnt % 2);
+  }
+  if (cnt === 10) {
+    if (isExistFile(gpio3)) {
+      fs.writeFileSync(dir + '/unexport', 3);
+    }
+    clearInterval(intervalFn);
+  }
 }, 500);
